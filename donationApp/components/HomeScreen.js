15d3ff6988c0734cb60f9b45db8b3eb106516892
Juan Zamudio <jzamudio14@gmail.com>
2023-09-30 18:13:38 -0400

    homescreen done

--- donationApp/components/HomeScreen.js
+++ donationApp/components/HomeScreen.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Logo from '../assets/images/logo.png'
 
 export default function HomeScreen() {
     return (
       <View style={styles.container}>
-        <Text>Welcome!</Text>
-        <Image style={styles.logo} source={Logo}/>
-        <Text>Join our community of givers and make a difference today!</Text>
-        <Button title="Get Started"/>
-        <Button title="Login"/>
+        <View style={{position: "absolute", top: 108}}>
+            <Text style={styles.welcomeText}>Welcome!</Text>
+            <Image style={styles.logo} source={Logo}/>
+        </View>
+        <View style={{top: 215, position: 'relative'}}>
+            <Text style={styles.joinText}>Join our community of givers and make a difference today!</Text>
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.buttonText}>Let's Get Started</Text>
+            </TouchableOpacity>
+        </View>
+        <View style={{borderColor: "black", borderWidth: 1, width: 332, position: "relative", top: 300}}/>
+        <Text style={styles.accountText}>Already have an account? <TouchableOpacity><Text style={styles.loginText}>Log In</Text></TouchableOpacity></Text>
         <StatusBar style="auto" />
       </View>
     );
@@ -24,7 +31,62 @@ export default function HomeScreen() {
     },
 
     logo: {
-      width: "281px",
-      height: "217px",
+      width: 281,
+      height: 217,
+      top: 73
+    },
+
+    welcomeText: {
+      color: "#314D89",
+      textAlign: "center",
+      fontFamily: "Roboto Slab",
+      fontSize: 45,
+      fontStyle: "normal",
+      fontWeight: 300,
+      lineHeight: 122.49 /* 55.121px */,
+    },
+
+    joinText:{
+      width: 322,
+      fontFamily: "Roboto",
+      fontSize: 18,
+      fontWeight: "400",
+      fontStyle: "normal",
+      textAlign: "center",
+      color: "#314D89",
+      lineHeight: 30
+    },
+
+    button: {
+      backgroundColor: "#314D89",
+      borderRadius: 25,
+      paddingVertical: 12,
+      paddingHorizontal: 12,
+      top: 58
+    },
+
+    buttonText: {
+      fontFamily: "Roboto Slab",
+      fontSize: 20,
+      fontWeight: "400",
+      fontStyle: "normal",
+      lineHeight: 20,
+      color: "#FFFFFF",
+      textAlign: "center"
+    },
+
+    accountText: {
+      color: "#314D89",
+      textAlign: "center",
+      fontFamily: "Roboto",
+      fontSize: 16,
+      fontWeight: 400,
+      top: 325
+    },
+
+    loginText: {
+      color: "black",
+      fontWeight: "bold",
+      top: 3
     }
   });

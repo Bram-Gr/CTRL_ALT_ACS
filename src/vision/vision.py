bb9f5e9c2193db76feac13a35258c835719a5f8c
Christerpher Hunter <djhunter67@gmail.com>
2023-09-30 17:48:34 -0400

    feat: initial progress w/ machine vision

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -1,50 +1,42 @@
-import torch
-from torchvision import models, transforms
-from PIL import Image
-import requests
-
-
-class ImageClassifier:
-    def __init__(self):
-        # Load a pre-trained ResNet model
-        self.model = models.resnet50(pretrained=True)
-        self.model.eval()  # Set the model to evaluation mode
-        self.labels_url = "https://raw.githubusercontent.com/anishathalye/imagenet-simple-labels/master/imagenet-simple-labels.json"
-        self.labels = requests.get(self.labels_url).json()
-
-        # Define image transformation for preprocessing
-        self.preprocess = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                                 0.229, 0.224, 0.225]),
-        ])
-
-    def classify_image_url(self, image_url):
-        # Load and preprocess the image from the URL
-        image = Image.open(requests.get(image_url, stream=True).raw)
-        input_tensor = self.preprocess(image)
-        input_batch = input_tensor.unsqueeze(0)
-
-        # Make predictions
-        with torch.no_grad():
-            output = self.model(input_batch)
-
-        _, predicted_idx = torch.max(output, 1)
-        predicted_label = self.labels[predicted_idx.item()]
-
-        return predicted_label
-
-
-def main():
-
-    # Replace with the URL of the image you want to classify
-    image_url = "https://example.com/your_image.jpg"
-    classifier = ImageClassifier()
-    predicted_label = classifier.classify_image_url(image_url)
-    print(f"The image is classified as: {predicted_label}")
-
-
-if __name__ == "__main__":
-    main()
+import cv2
+from matplotlib import pyplot as plt
+  
+# Opening image
+img = cv2.imread("good_couch.jpg")
+  
+# OpenCV opens images as BRG 
+# but we want it as RGB We'll 
+# also need a grayscale version
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  
+  
+# Use minSize because for not 
+# bothering with extra-small 
+# dots that would look like STOP signs
+stop_data = cv2.CascadeClassifier('stop_data.xml')
+  
+found = stop_data.detectMultiScale(img_gray, 
+                                   minSize =(20, 20))
+  
+# Don't do anything if there's 
+# no sign
+amount_found = len(found)
+  
+if amount_found != 0:
+      
+    # There may be more than one
+    # sign in the image
+    for (x, y, width, height) in found:
+          
+        # We draw a green rectangle around
+        # every recognized sign
+        cv2.rectangle(img_rgb, (x, y), 
+                      (x + height, y + width), 
+                      (0, 255, 0), 5)
+          
+# Creates the environment of 
+# the picture and shows it
+plt.subplot(1, 1, 1)
+plt.imshow(img_rgb)
+plt.savefig("test_images.jpg")

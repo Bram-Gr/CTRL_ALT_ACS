7343a412d1414ef6cc259d4df2dcc78da1b06010
Christerpher Hunter <djhunter67@gmail.com>
2023-09-30 21:18:36 -0400

    add: remove darknet model

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -1,21 +1,53 @@
 import subprocess
 import pathlib
+from colorama import Fore as F
+import time
+import os
+
+R = F.RESET
 
 
-def detect(config, weights, image):
+def detect(exe, config, weights, image):
     """Execute ./darknet detect {config} {weights} {image}"""
     result = subprocess.run(
-        ['./darknet', 'detect', config, weights, image], stdout=subprocess.PIPE)
+        [f"./{exe}", "detect", config, weights, image],
+        stdout=subprocess.PIPE
+    )
+
     return result.stdout.decode('utf-8')
+
+
+def run_darknet(image):
+    """Run darknet on the image"""
+
+    ROOT = pathlib.Path(__file__).parent.parent.parent.absolute()
+    # Change directory to darknet
+    os.chdir(ROOT / "static/assets/darknet")
+
+    exe = "darknet"
+    config = "cfg/yolov3.cfg"
+    weights = "yolov3.weights"
+
+    # Time the detection
+    start = time.time()
+    result = detect(exe, config, weights, image)
+    end = time.time()
+
+    print(f"{F.YELLOW}Time:{R} {end - start}")
+
+    return result
 
 
 def main():
 
-    ROOT = pathlib.Path(__file__).parent.parent.parent.absolute()
-    config = str(ROOT / "static/assets/darknet/cfg/yolov3.cfg")
-    # weights = 'yolov3.weights'
-    # image = 'data/dog.jpg'
-    # print(detect(config, weights, image))
+    images = [
+        "../../../static/img/good_couch.jpg",
+        "../../../static/img/green_couch.png",
+        "../../../static/img/brown_couch.png"
+    ]
+
+    for image in images:
+        run_darknet(image)
 
 
 if __name__ == "__main__":

35b669da143dd312aa4dc649666b27617e4c282b
Bram <b93ram@gmail.com>
2023-09-30 21:44:03 -0400

    endpoints for creating a donation and retrieving the donations

--- .idea/data/schema.sql
+++ .idea/data/schema.sql
@@ -1,36 +1,34 @@
-BEGIN TRANSACTION;
+-- BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS donations_status CASCADE;
-DROP TABLE IF EXISTS donations CASCADE;
-DROP TABLE IF EXISTS managers CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS donations_status;
+-- DROP TABLE IF EXISTS donations;
+-- DROP TABLE IF EXISTS managers;
+-- DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50),
-    password VARCHAR(50)
+    password VARCHAR(50),
+    user_id INTEGER PRIMARY KEY
 );
 
-CREATE TABLE managers (
-    manager_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS managers (
     username VARCHAR(50),
-    password VARCHAR(50)
+    password VARCHAR(50),
+    manager_id INTEGER PRIMARY KEY
 );
 
-CREATE TABLE donations (
+CREATE TABLE IF NOT EXISTS donations (
     donation_id SERIAL PRIMARY KEY,
     description VARCHAR(250),
     image BYTEA NOT NULL,
     user_id INTEGER,
-    manager_id INTEGER,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
-    CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES managers(manager_id)
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE donation_status (
+CREATE TABLE IF NOT EXISTS donation_status (
     donation_id INTEGER,
     status VARCHAR(250),
     CONSTRAINT fk_donation_id FOREIGN KEY (donation_id) REFERENCES donations(donation_id)
 );
 
-COMMIT TRANSACTION;
+-- COMMIT TRANSACTION;

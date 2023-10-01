eb0b6b71d83991335e7871b11f4b9bd15ee61fc7
Christerpher Hunter <djhunter67@gmail.com>
2023-09-30 23:34:55 -0400

    wip: finalizing the model

--- src/vision/vision.py
+++ src/vision/vision.py
@@ -2,12 +2,12 @@ import mediapipe as mp
 import pathlib
 
 ROOT = pathlib.Path(__file__).parent.parent.parent.resolve()
-image_path = ROOT / "static/img/desk.png"
+image_path = ROOT / "static/img/green_couch.png"
 
 mp_img = mp.Image.create_from_file(str(image_path))
 
 
-tflite = ROOT / "static/assets/ssd_mobilenet_v2.tflite"
+tflite = ROOT / "static/assets/efficientdet_lite2.tflite"
 
 BaseOptions = mp.tasks.BaseOptions
 ObjectDetector = mp.tasks.vision.ObjectDetector
@@ -16,14 +16,26 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 options = ObjectDetectorOptions(
     base_options=BaseOptions(model_asset_path=str(tflite)),
-    max_results=5,
+    max_results=15,
     running_mode=VisionRunningMode.IMAGE)
+
 
 results = None
 with ObjectDetector.create_from_options(options) as detector:
-    # The detector is initialized. Use it here.
+    # with ImageClassifier.create_from_options(options) as classifier:
     results = detector.detect(mp_img).detections
+    # results = classifier.classify(mp_img)
+
+result_types = {}
 
 for detection in results:
-    print(f"Label: {str(detection.__dict__['categories'][0].score)} - "
-          f"Label: {str(detection.__dict__['categories'][0].category_name)}")
+    print(f"{detection.__dict__['categories'][0].score} - "
+          f"{str(detection.__dict__['categories'][0].category_name)}")
+    result_types.update({str(detection.__dict__['categories'][0].score):
+                         detection.__dict__['categories'][0].category_name})
+
+# Filter the results to only include the highest confidence detections.
+highest_confidence_detection = max(results, key=lambda x: x.keys())
+
+
+print(f"Detection class name: {highest_confidence_detection}")
